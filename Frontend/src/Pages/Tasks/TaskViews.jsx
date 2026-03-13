1e7bb4921b693e21c30754cbadec982a34c06fc6
Santhakumar99 <santhakumar9399@gmail.com>
2026-03-13 18:00:04 +0530

    fix css path

--- Frontend/src/Pages/Tasks/TaskViews.jsx
+++ Frontend/src/Pages/Tasks/TaskViews.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import "../tasks/taskView.css";
+import "../Tasks/TaskView.css";
 import { useLocation, useNavigate, useParams } from "react-router-dom";
 import axios from "axios";
 import { formatDate } from "../CommonComponents/DateFormat.jsx";
